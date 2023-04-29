30f965afd6bcd6f8e1595ed07ef30dc5a9485579
Cesar Aldair Garcia Velez <aldairgarciavelez1@gmail.com>
2023-04-29 10:53:58 -0600

    Ahora promedia datos y el numero de datos promediados funciona con un potenciometro

--- MicrometroLaser.ino
+++ MicrometroLaser.ino
@@ -2,7 +2,15 @@
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
+int counter = 0;
+long sum = 0;
+
+int ACCURACY_PIN = A7;
+int DEFAULT_ACCURACY = 29;
+int accuracy = 0;
+
 void setup() {
+  delay(50);
   Serial.begin(115200);
 
   // wait until serial port opens for native USB devices
@@ -11,26 +19,40 @@ void setup() {
   }
   
   Serial.println("Adafruit VL53L0X test");
-  if (!lox.begin()) {
+  if (!lox.begin(0x29,true)) {
     Serial.println(F("Failed to boot VL53L0X"));
     while(1);
   }
   // power 
-  Serial.println(F("VL53L0X API Simple Ranging example\n\n")); 
+  Serial.println(F("VL53L0X API Simple Ranging example\n")); 
+
+  pinMode(ACCURACY_PIN,INPUT);
+
+  // Valor de la precision
+  int a = analogRead(A7);
+  accuracy = (a / 2) + DEFAULT_ACCURACY;
+  float string_accuracy = accuracy / 5.4;
+  Serial.print("Midiendo con una precision del ");
+  Serial.print(string_accuracy);
+  Serial.print(" % ( ");
+  Serial.print(accuracy);
+  Serial.println(" datos)");
 }
 
 
 void loop() {
   VL53L0X_RangingMeasurementData_t measure;
-    
-  Serial.print("Reading a measurement... ");
   lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
 
-  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
-    Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
-  } else {
-    Serial.println(" out of range ");
+  int mm = measure.RangeMilliMeter;
+
+  if(counter < accuracy){
+    counter++;
+    sum += mm;
+  }else{
+    float avarage = sum / (float) counter;
+    Serial.print("Distancia: ");
+    Serial.println(avarage);
+    while(true);
   }
-    
-  delay(100);
 }
